b069aa951e37a88a1c5e3ff3ee24347c49f0aa36
Ascold1 <ascold96@gmail.com>
2017-07-10 17:01:44 +0300

    upd 1

--- app/js/common.js
+++ app/js/common.js
@@ -5,9 +5,25 @@ $(document).ready(function(){
        infinite: true,
        slidesToShow: 2,
        slidesToScroll: 1,
-       arrow: true,
+       arrows: true,
        prevArrow:'<img src="img/prev.png" alt="<" class="slick-prev" >',
        nextArrow:'<img src="img/next.png" alt="<" class="slick-next">',
+       responsive: [
+            {
+              breakpoint: 1024,
+              settings: {
+                   slidesToShow: 2,
+                   slidesToScroll: 1,
+                   arrows: false,
+              },
+              breakpoint: 768,
+              settings: {
+                   slidesToShow: 1,
+                   slidesToScroll: 1,
+                  arrows: false,
+              }
+            },
+        ]
        
    });
     $(".gallery_link").fancybox();
